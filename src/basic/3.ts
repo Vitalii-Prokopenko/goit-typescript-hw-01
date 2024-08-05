f1b93501b484fac61dafc84b6f85cde0f6874d34
Vitalii Prokopenko <doc8259@gmail.com>
2024-08-05 20:26:20 +0300

    adds works

---
+++ src/basic/3.ts
@@ -0,0 +1,6 @@
+// Створіть змінну, яка може містити або рядок, або число (union type)? 
+// Також, оголосіть змінну, яка може містити лише одне з двох 
+// можливих рядкових значень: 'enable' або 'disable' (literal type).
+
+let varUnionType: string | number;
+type varLiteralType = 'enable' | 'disable';
---
+++ src/generics/3.ts
@@ -0,0 +1,11 @@
+// У вас є функція merge, яка поєднує два об'єкти.
+// Використовуйте generics, щоб вказати,
+// що ці об'єкти можуть бути будь-якого типу.
+
+// function merge (objA, objB) {
+//   return Object.assign(objA, objB);
+// }
+
+function merge(objA: object, objB: object): object {
+  return Object.assign(objA, objB);
+}
